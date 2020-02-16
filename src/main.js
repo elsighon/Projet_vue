08b6629f0a907bda5f6efd61127b457854e029ea
elsighon <55087833+elsighon@users.noreply.github.com>
2020-02-16 22:12:20 +0100

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import router from './router'
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
